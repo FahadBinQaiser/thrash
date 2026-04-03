8f6163500378d2a374176bbda7fcb81019dd5150
Fahad <fahadbinqaiser3@gmail.com>
2026-04-03 23:45:15 +0500

    Button seperated

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { sora } from "@/app/layout";
+import Button from "@/components/ui/Button";
 
 export default function Hero() {
   return (
@@ -23,12 +24,8 @@ export default function Hero() {
           tone and the same BS anymore.
         </p>
         <div className="flex flex-col sm:flex-row gap-4">
-          <button className="bg-[#ef233c] text-[#edf2f4] font-semibold px-8 py-4 rounded-full hover:bg-[#d90429] transition">
-            Try Your First Post Free
-          </button>
-          <button className="border-b-5 border-t border-[#edf2f4] text-[#edf2f4] px-8 py-4 rounded-full hover:bg-[#8d99ae]/10 transition">
-            See a Real Example
-          </button>
+          <Button variant="primary" label="Try Your First Post Free" />
+          <Button variant="secondary" label="See a Real Example" />
         </div>
       </div>
     </section>
---
+++ components/ui/Button.tsx
@@ -0,0 +1,27 @@
+interface ButtonProps {
+  label: string;
+  variant: "primary" | "secondary";
+  onClick?: () => void;
+}
+
+export default function Button({ label, variant, onClick }: ButtonProps) {
+  if (variant === "primary") {
+    return (
+      <button
+        onClick={onClick}
+        className="bg-[#ef233c]  cursor-pointer text-[#edf2f4] font-semibold px-8 py-4 rounded-full hover:bg-[#d90429] transition"
+      >
+        {label}
+      </button>
+    );
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="border-5 border-[#edf2f4] cursor-pointer text-[#edf2f4] px-8 py-4 rounded-full hover:bg-[#8d99ae]/10 transition"
+    >
+      {label}
+    </button>
+  );
+}
